22351c84c879726f85c27899c5e89db5766202e8
liubo <1353648067@qq.com>
2022-04-25 21:15:27 +0800

    修bug：释放内存和空指针

--- 09_为何继承/main.cpp
+++ 09_为何继承/main.cpp
@@ -8,5 +8,10 @@ int main()
     stu->setAge(15);
     stu->setId(17);
     cout << "My name is " << stu->getName() << ", I'm " << stu->getAge() << " years old, my id is " << stu->getId();
+
+    //释放内存
+    delete stu;
+    //释放空指针
+    stu = nullptr;
     return 0;
 }

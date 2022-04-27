bda3193619f247ae4f72f6f70077fb564abd2d6a
liubo <1353648067@qq.com>
2022-04-27 09:55:18 +0800

    多继承与多重继承概念示例

---
+++ 12_多继承与多重继承/多继承与多重继承概念/sample1.cpp
@@ -0,0 +1,10 @@
+class A
+{
+};
+class B
+{
+};
+//多继承
+class C : public A, public B
+{
+};
---
+++ 12_多继承与多重继承/多继承与多重继承概念/sample2.cpp
@@ -0,0 +1,10 @@
+class A
+{
+};
+class B : public A
+{
+};
+//多重继承:C继承自B,而B又继承自A
+class C : public B
+{
+};

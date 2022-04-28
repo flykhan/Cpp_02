634a4350c7cfba1dbc76aa9db69ad472389f78f2
liubo <1353648067@qq.com>
2022-04-28 16:39:35 +0800

    菱形继承

---
+++ 13_虚继承/菱形继承/Student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+#include <string>
+#include "Person.h"
+using namespace std;
+class Student : public Person
+{
+protected:
+    int age;
+
+public:
+    Student(const string &name = "wanger", int age = 15);
+    ~Student();
+};
+#endif
